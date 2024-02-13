70c4e41cbd28ab6775091838af94831960e5cac1
Bambitto <pawel.borkowski.kontakt@gmail.com>
2024-02-13 17:11:08 +0100

    add genre filter

--- BookWeb/WebPage/src/app/book-list/book-list.component.ts
+++ BookWeb/WebPage/src/app/book-list/book-list.component.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common'
 import { NgOptimizedImage } from '@angular/common'
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipListboxChange, MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, MatIconModule, MatButtonModule],
+  imports: [CommonModule, NgOptimizedImage, MatIconModule, MatButtonModule, MatChipsModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
@@ -17,104 +18,106 @@ export class BookListComponent implements OnInit {
     {
       id: 1,
       title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
       imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
       isFavorite: true,
     },
     {
-      id: 2,
+      id: 1,
       title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
+      author: 'J. N. Bowling',
+      genre: 'Kryminał',
       imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
-    },
-    {
-      id: 3,
-      title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
-      imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
+      isFavorite: true,
     },
     {
       id: 1,
       title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
       imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
-    },
-    {
-      id: 2,
-      title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
-      imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
-    },
-    {
-      id: 3,
-      title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
-      imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
+      isFavorite: true,
     },
     {
       id: 1,
       title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
       imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: true
-    },
-    {
-      id: 2,
-      title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
-      imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
-    },
-    {
-      id: 3,
-      title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
-      imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
+      isFavorite: true,
     },
     {
       id: 1,
       title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
       imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
+      isFavorite: true,
     },
     {
-      id: 2,
+      id: 1,
       title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
       imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
+      isFavorite: true,
     },
     {
-      id: 3,
+      id: 1,
       title: 'Beautiful Beach House',
-      location: 'California, USA',
-      price: 200,
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
       imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
-      isFavorite: false
+      isFavorite: true,
     },
+    {
+      id: 1,
+      title: 'Beautiful Beach House',
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
+      imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
+      isFavorite: true,
+    },
+    {
+      id: 1,
+      title: 'Beautiful Beach House',
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
+      imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
+      isFavorite: true,
+    },
+    {
+      id: 1,
+      title: 'Beautiful Beach House',
+      author: 'J. N. Bowling',
+      genre: 'Komedia',
+      imageUrl: 'https://sukces.pl/sklep/wp-content/uploads/2019/02/ksiazka_rusz_dupe.jpg',
+      isFavorite: true,
+    },
+
   ];
+  showedBooks: Book[] = this.books;
+  public genres: string[] = ['Komedia', 'Kryminał', 'Powieść', 'Sci-finction', 'Kryminał', 'Powieść', 'Sci-finction', 'Kryminał', 'Powieść', 'Sci-finction']
+  selectedGenre = "";
 
   constructor() { }
 
   ngOnInit(): void {
 
+  }
+
+  onSelection(event: MatChipListboxChange) {
+    const selected = event.value;
+    if (selected.length) {
+      // Assuming single selection, adjust if multiple
+      this.selectedGenre = selected;
+      this.showedBooks = this.books.filter(book => book.genre === this.selectedGenre);
+    } else {
+      this.selectedGenre = "";
+    }
+    
+
+    console.log(this.selectedGenre)
   }
 }
